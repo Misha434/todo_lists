8dc1d346d8a420bf3066aeab950b4a4a06b9d8b4
misha434 <missymouse434@gmail.com>
2021-10-13 14:44:35 +0900

    Try stateful conponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,19 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-        <p>So you were probably born in {bornYear()}</p>
-      </p>
-    </div>
-  )
-}
-
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [ counter, setCounter] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter)
 
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </>
+    <div>{counter}</div>
   )
 }
 
