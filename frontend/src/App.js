ce2b6b7369c15a23f202d95f3b8f6ba44d1864eb
misha434 <missymouse434@gmail.com>
2021-10-14 13:43:07 +0900

    Destructing code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,42 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const Display = ({counter}) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [ counter, setCounter] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}> 
-        zero
-      </button>
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='+'
+      />
+      <Button
+        onClick={setToZero}
+        text='0'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='-'
+      />   
     </>
   )
 }
