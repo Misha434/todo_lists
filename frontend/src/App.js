1afca0eedc2b588b09fb76b7d1d643f557920b18
misha434 <missymouse434@gmail.com>
2021-10-16 21:41:05 +0900

    Function that returns a function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,54 +2,19 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <>
-        the app is used bt pressing the buttons
-      </>
-    )
-  }
-  return (
-    <>
-      button press history: {props.allClicks.join(' ')}
-    </>
-  )
-}
-
-const Button = (props) => { 
-  console.log('props value is ', props)
-  const { onClick, text } = props
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
-
-
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"))
-    setRight(right + 1)
+  const setToValue = (newValue) => () => {
+    setValue(newValue)
   }
 
   return (
     <>
-      {left}
-      <Button handleClick={handleLeftClick} text="left" />
-      <Button handleClick={handleRightClick} text="right" />
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={() => setToValue(1000)}>thousand</button>
+      <button onClick={() => setToValue(0)}>reset</button>
+      <button onClick={() => setToValue(value + 1)}>increment</button>
     </>
   )
 }
