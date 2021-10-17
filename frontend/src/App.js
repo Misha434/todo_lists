a22e7243759477e6390b0ae45c6862f380cb97e4
misha434 <missymouse434@gmail.com>
2021-10-17 13:40:50 +0900

    Destruct Note component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 import Note from './components/Note';
 import './App.css';
 
-const App = (notes) => {
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map(note => 
-          <Note key={note.id} note={note.content} />
+          <Note key={note.id} note={note} />
         )}
       </ul>
     </div>
