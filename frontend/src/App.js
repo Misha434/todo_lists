9fb803bbd61ecef274911f57d5379583d8e74f56
misha434 <missymouse434@gmail.com>
2021-10-20 22:55:45 +0900

    Recive api-server json-data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,117 +1,26 @@
 // import logo from './logo.svg';
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Note from './components/Note'
-import Notification from './components/Notification'
 
 import './App.css'
 
-import noteService from './services/notes'
-
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
-
+  const [todos, setTodos] = useState([])
 
   useEffect(() => {
-    noteService
-      .getAll()
-      .then(initalNotes => {
-        setNotes(initalNotes)
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/api/v1/todos')
+      .then(response => {
+        console.log('promise fulfilled')
+        setTodos(response.data)
       })
-    }, [])
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() > 0.5,
-    }
-
-    noteService
-      .create(noteObject)
-      .then(returnedNote => {
-        setNotes(notes.concat(returnedNote))
-        setNewNote('')
-      })
-  }
-
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
+  }, [])
   
-    noteService
-      .update(id, changedNote).then(returnNote => {
-        setNotes(notes.map(note => note.id !== id ? note : returnNote))
-      })
-      .catch(error => {
-        setErrorMessage(
-          `Note '${note.content}' was already removed from server `
-        )
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-        setNotes(notes.filter(n => n.id !== id))
-      })
-  }
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
-
-  const changedNote = { ...notes, important: !notes.important }
-
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
-  
-  const Footer = () => {
-    const footerStyle = {
-      color: 'green',
-      fontStyle: 'italic',
-      fontSize: 16
-    }
-    return (
-      <div style={footerStyle}>
-        <br />
-        <em>Note app</em>
-      </div>
-    )
-  }
-
   return (
-    <div>
-      <h1>Notes</h1>
-      <Notification message={errorMessage} />
-      <>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? "important" : "all"}
-        </button>
-      </>
-      <ul>
-        {notesToShow.map(note => 
-          <Note 
-            key={note.id}
-            note={note}
-            toggleImportance={()=> toggleImportanceOf(note.id)}
-          />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button> 
-      </form>
-      <Footer />
-    </div>
+    <>
+    </>
   )
 }
-
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-import axios from 'axios'
-
-axios.get('http://localhost:3001/notes').then(response => {
-  // const notes = response.data
-  ReactDOM.render(<App />, document.getElementById('root'));
-  })
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
